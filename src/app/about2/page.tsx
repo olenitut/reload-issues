bc853f09b0a5699cb8cee5bfbbfc8136c02ebd16
Alex Chernichenko <alexchernichenko18@gmail.com>
2023-10-14 07:00:50 +0300

    fix

---
+++ src/app/about2/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function About2() {
+  return (
+    <div>
+      <h1>This is About2 Page</h1>
+      <Link href='/'>Go to Home Page</Link>
+    </div>
+  )
+}
